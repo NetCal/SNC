bf7095c7256744eb3c8c45216726afa5cf28d12b
jerous <jerous@gmail.com>
2019-04-05 20:19:02 +0200

    detect version
    check for jar-file rather than target directory

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-if [ ! -d target ]; then
+# This script runs the jar-file.
+# If the jar-file is not present, it will compile using maven.
+# Note that if the source has changed, the project will not recompile until the jar-file
+# is removed.
+
+set -ue
+
+VERSION=$(cat pom.xml | paste -s - | grep -Eoi '<groupId>org.networkcalculus.snc.*?/version' | sed 's:.*version>\(.*\)</version.*:\1:g')
+TARGET_JAR="target/SNC-${VERSION}-jar-with-dependencies.jar"
+if [ ! -f "$TARGET_JAR" ]; then
 	mvn package
 fi
-java -jar target/SNC-2.1.0-SNAPSHOT-jar-with-dependencies.jar
+
+java -jar "$TARGET_JAR"
+
